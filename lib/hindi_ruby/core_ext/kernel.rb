2bd0b31ce8819fcee864bde284295b84b0fc8228
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-05-15 23:55:52 +0900

    Add basic aliases

--- lib/hindi_ruby/core_ext/kernel.rb
+++ lib/hindi_ruby/core_ext/kernel.rb
@@ -1,3 +1,11 @@
 module Kernel
   alias छापो puts
+  alias बंधन binding
+  alias डब्बा_है? block_given?
+  alias चक्र loop
+  alias लीखो print
+  alias कोईभी rand
+  alias सो sleep
+  alias सीस्टम system
+  alias चेतावणी️ warn
 end
