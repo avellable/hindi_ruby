91a4e65aba60ebb888de332293438d4865e4494f
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-05-16 00:01:50 +0900

    Add basic enumerable aliases

---
+++ lib/hindi_ruby/core_ext/enumerable.rb
@@ -0,0 +1,11 @@
+module Enumerable
+  alias कोई? any?
+  alias िगनती count
+  alias गीराअो drop
+  alias हर_तुकडा each_slice
+  alias ढुंडौ find
+  alias घुसाअो inject
+  alias उलटा_हरएक reverse_each
+  alias छानो sort
+  alias लो take
+end
